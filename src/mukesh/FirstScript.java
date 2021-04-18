768bcf85f2913782c0eea85d7627fca8d091bd62
msdhisale <technolearnmd@gmail.com>
2021-04-18 14:56:26 +0530

    Removed comment pageSource code line in FirstScript.java

--- src/mukesh/FirstScript.java
+++ src/mukesh/FirstScript.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FirstScript {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -14,15 +13,11 @@ public class FirstScript {
         String baseUrl = "http://demo.guru99.com/test/newtours/";
         String expectedTitle = "Welcome: Mercury Tours";
         String actualTitle = "";
-        String pageSourceCode;
         // launch Chrome and direct it to the Base URL
         driver.get(baseUrl);
 
         // get the actual value of the title
         actualTitle = driver.getTitle();
-        //pageSourceCode = driver.getPageSource();
-        // get the page source code
-        //System.out.println(pageSourceCode);
         /*
          * compare the actual title of the page with the expected one and print
          * the result as "Passed" or "Failed"
